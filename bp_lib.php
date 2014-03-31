118d8b8b9fb7e9cd1f9517e96a6facba1a17a868
Rich Morgan <ionux@users.noreply.github.com>
2014-03-31 11:39:20 -0400

    Added new HTTP header for version tracking

--- bp_lib.php
+++ bp_lib.php
@@ -19,6 +19,7 @@ function bpCurl($url, $apiKey, $post = false) {
 		'Content-Type: application/json',
 		"Content-Length: $length",
 		"Authorization: Basic $uname",
+		'X-BitPay-Plugin-Info: woocommerce1.2',
 		);
 
 	curl_setopt($curl, CURLOPT_PORT, 443);
