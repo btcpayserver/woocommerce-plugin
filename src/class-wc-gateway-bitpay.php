64ca353889278fd95f18eceb787c5ef3cbc637d4
Chris Kleeschulte <chrisk@bitpay.com>
2015-12-04 11:12:52 -0500

    Updated the version numbers in the main class.

--- src/class-wc-gateway-bitpay.php
+++ src/class-wc-gateway-bitpay.php
@@ -6,7 +6,7 @@
     Author:      bitpay
     Author URI:  https://bitpay.com
 
-    Version:           2.2.8
+    Version:           2.2.9
     License:           Copyright 2011-2014 BitPay Inc., MIT License
     License URI:       https://github.com/bitpay/woocommerce-plugin/blob/master/LICENSE
     GitHub Plugin URI: https://github.com/bitpay/woocommerce-plugin
@@ -1393,7 +1393,7 @@ function woocommerce_bitpay_activate()
             }
         }
 
-        update_option('woocommerce_bitpay_version', '2.2.8');
+        update_option('woocommerce_bitpay_version', '2.2.9');
 
     } else {
         // Requirements not met, return an error message
