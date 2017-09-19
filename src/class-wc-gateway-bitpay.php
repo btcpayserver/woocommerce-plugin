808bf434946b3db1da8e9f6cf9dac5b8a8a8382c
Pieter Poorthuis <pieter@bitpay.com>
2017-09-19 17:33:38 +0200

    Update class-wc-gateway-bitpay.php

--- src/class-wc-gateway-bitpay.php
+++ src/class-wc-gateway-bitpay.php
@@ -7,8 +7,8 @@
     Text Domain: bitpay
     Author URI:  https://bitpay.com
 
-    Version:           2.2.11-beta
-    License:           Copyright 2011-2014 BitPay Inc., MIT License
+    Version:           2.2.12
+    License:           Copyright 2011-2017 BitPay Inc., MIT License
     License URI:       https://github.com/bitpay/woocommerce-plugin/blob/master/LICENSE
     GitHub Plugin URI: https://github.com/bitpay/woocommerce-plugin
  */
