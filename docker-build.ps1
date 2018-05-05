c70d4aee57e56ab1380fcc6fb0a49fcf6220f2a0
nicolas.dorier <nicolas.dorier@gmail.com>
2018-05-06 01:08:07 +0900

    update .ps1

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,4 +1,5 @@
 rm dist -Force -Recurse
+mkdir "dist"
 docker build -t woocommerce_bitpay . 
 docker run -ti -v "$pwd/dist:/app/dist" --rm woocommerce_bitpay
 echo "Output available in $pwd\dist\btcpay-for-woocommerce.zip"
