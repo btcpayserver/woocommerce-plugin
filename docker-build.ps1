03718032a0287131f895f880b3432b067e4d5b8d
Kukks <evilkukka@gmail.com>
2018-11-26 13:03:29 +0100

    Revert "new root folder so that users can upload plugin without issue"
    This reverts commit f9bc40dd2c2e57fae0c49170f62961afed959b83.

--- docker-build.ps1
+++ docker-build.ps1
@@ -2,5 +2,5 @@ rm dist -Force -Recurse
 mkdir "dist"
 docker build -t woocommerce_bitpay . 
 docker run -ti -v "$pwd/dist:/app/dist" --rm woocommerce_bitpay
-echo "Output available in $pwd\dist\btcpay-woocommerce.zip"
-Copy-Item "$pwd\dist\btcpay-woocommerce\*" "$pwd\woocommerce\html\wp-content\plugins\btcpay-woocommerce" -Force -Recurse
+echo "Output available in $pwd\dist\btcpay-for-woocommerce.zip"
+Copy-Item "$pwd\dist\btcpay-for-woocommerce\*" "$pwd\woocommerce\html\wp-content\plugins\btcpay-for-woocommerce" -Force -Recurse
