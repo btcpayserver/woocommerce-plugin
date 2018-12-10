e7bf9118bb7af8463d5ecac19e7c65586672b0d6
Kukks <evilkukka@gmail.com>
2018-12-10 10:31:40 +0100

    remove plugin header for old file

--- src/class-wc-gateway-bitpay.php
+++ src/class-wc-gateway-bitpay.php
@@ -1,17 +1,3 @@
 <?php 
-/*
-    Plugin Name: BTCPay for WooCommerce
-    Plugin URI:  https://wordpress.org/plugins/btcpay-for-woocommerce
-    Description: Enable your WooCommerce store to accept Bitcoin with BTCPay.
-    Author:      BTCPay
-    Text Domain: BTCPay
-    Author URI:  https://github.com/btcpayserver
-
-    Version:           3.0.4
-    License:           Copyright 2011-2018 BTCPay & BitPay Inc., MIT License
-    License URI:       https://github.com/btcpayserver/woocommerce-plugin/blob/master/LICENSE
-    GitHub Plugin URI: https://github.com/btcpayserver/woocommerce-plugin
-*/
-
     require_once __DIR__ ."/class-wc-gateway-btcpay.php" 
 ?>
